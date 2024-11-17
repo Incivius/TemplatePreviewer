5617f1e446bc9e83f1c3fa9b079168fa735c611a
Incivius <marcos.santos232@fatec.sp.gov.br>
2024-11-17 11:36:37 -0300

    feat: implements function get all and CORS

--- api/app/controller/template.py
+++ api/app/controller/template.py
@@ -1,10 +1,8 @@
 from fastapi import HTTPException
-from app.models.template import create_template, get_template
+from app.models.template import create_template, get_all_templates, get_template
 from app.entity.template import TemplateIn, TemplateOut
+from typing import List
 
-# Remova a criação do FastAPI aqui
-
-# A rota já estará registrada em Main.py, então não há necessidade de repetir isso aqui
 async def create(template: TemplateIn) -> TemplateOut:
     template_created = await create_template(template)
     return template_created
@@ -14,3 +12,9 @@ async def read(template_id: str) -> TemplateOut:
     if template is None:
         raise HTTPException(status_code=404, detail="Template not found")
     return template
+
+async def read_all() -> List[TemplateOut]:
+    templates = await get_all_templates()
+    if not templates:
+        raise HTTPException(status_code=404, detail="No templates found")
+    return templates
--- api/app/models/template.py
+++ api/app/models/template.py
@@ -1,6 +1,7 @@
 from app.database.db import db
 from app.entity.template import TemplateIn, TemplateOut
 from bson import ObjectId
+from typing import List
 
 async def create_template(template: TemplateIn) -> TemplateOut:
     template_dict = template.dict()
@@ -15,3 +16,12 @@ async def get_template(template_id: str) -> TemplateOut:
     if template:
         return TemplateOut(id=str(template["_id"]), **template)
     return None
+
+async def get_all_templates() -> List[TemplateOut]:
+    templates_cursor = db.templates.find()
+    templates = []
+    
+    async for template in templates_cursor:
+        templates.append(TemplateOut(id=str(template["_id"]), **template))
+    
+    return templates
